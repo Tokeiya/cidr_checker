ff0b3c1d839643572ae2bf0e37216719854bd6ba
Tokeiya <netseed@seedcreate.onmicrosoft.com>
2023-10-06 21:38:56 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use crate::cidr::Cidr;
-use crate::ipv4::IPv4;
-use crate::ipv4_mask::IPv4Mask;
+use std::env;
 
 mod cidr;
 mod cidr_error;
@@ -8,19 +6,12 @@ mod ipv4;
 mod ipv4_error;
 mod ipv4_mask;
 mod ipv4_mask_error;
+mod ipv4_network_range;
 
 fn main() {
-	let network = Cidr::new(
-		IPv4::try_from("192.168.112.0").unwrap(),
-		IPv4Mask::new(20).unwrap(),
-	)
-	.unwrap();
+	let args: Vec<_> = env::args().collect();
 
-	let verify_target = IPv4::try_from("192.168.121.101").unwrap();
-
-	println!("NetworkRange:{:?}", network);
-	println!("NetworkAddress:{}", network.address());
-	println!("SubnetMask:{}", network.subnet_mask().mask_address());
-	println!("VerifyTarget:{}", verify_target);
-	println!("IsContain:{}", network.contains(&verify_target));
+	for i in 0..args.len() {
+		println!("{}:{}", i, args[i]);
+	}
 }
